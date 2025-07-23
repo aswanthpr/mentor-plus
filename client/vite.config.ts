b56fa1910f821b3ab5c4363b8628279da06003ab
aswanthpr <aswanthpr555@gmail.com>
2025-07-23 15:36:18 +0530

    add config pwa

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 
-
 export default defineConfig({
   plugins: [
     react(),
@@ -24,21 +23,21 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "pwa-mntpls.png",
-            sizes: "192x192",
-            type: "image/png",
+            src: 'mentor-plus.png',
+            sizes: '192x192',
+            type: 'image/png'
           },
           {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
+            src: 'mentor-plus.png',
+            sizes: '512x512',
+            type: 'image/png'
           },
           {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
+            src: 'mentor-plus.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
         ],
       },
     }),
